21ccf81c2bc174fc1420447f7ec35d29a2933514
Ruari Hodgin <ruarihodgin99@outlook.com>
2025-05-16 12:07:31 +0200

    project list

---
+++ projects_model.py
@@ -0,0 +1,56 @@
+# projects_model.py
+
+import json
+import time
+
+class ProjectModel:
+
+    db = {}
+
+    def __init__(self, id_=None, title=None, date=None, description=None, keywords=None, image=None):
+        self.id = id_
+        self.title = title
+        self.date = date
+        self.description = description
+        self.keywords = keywords
+        self.image = image
+
+    def __str__(self):
+        return json.dumps(self.__dict__, ensure_ascii=False)
+
+    @classmethod
+    def load_db(cls):
+        with open('projects.json', 'r') as projects_file:
+            projects = json.load(projects_file)
+            cls.db.clear()
+            for c in projects:
+                cls.db[c['id']] = ProjectModel(c['id'], c['title'], c['date'], c['description'], c['keywords'], c['image'])
+
+    @classmethod
+    def count(cls):
+        time.sleep(2)
+        return len(cls.db)
+    
+
+    @classmethod
+    def find(cls, id_):
+        id_ = int(id_)
+        c = cls.db.get(id_)
+
+        return c
+
+    @classmethod
+    def all(cls):
+        return list(cls.db.values())
+
+    @classmethod
+    def search(cls, text):
+        result = []
+        for c in cls.db.values():
+            match_title = c.title is not None and text in c.title
+            match_date = c.date is not None and text in c.date
+            match_description = c.description is not None and text in c.description
+            match_keywords = c.keywords is not None and text in c.keywords
+            if match_title or match_date or match_description or match_keywords:
+                result.append(c)
+        return result
